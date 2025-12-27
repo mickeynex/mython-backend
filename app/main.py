6794a842bb83075ecf309a960172675f10d16317
mickey nex <nexmickey@gmail.com>
2025-12-27 14:52:41 +0530

    enable CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from fastapi.websockets import WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from .db import get_db
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
